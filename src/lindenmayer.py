36355b10b9de485f71c689b242def00ff5338a37
Jake Cariello <jakecariello@Jakes-MacBook-Pro.local>
2018-10-10 00:05:12 -0400

    late night edits to the L system

--- src/lindenmayer.py
+++ src/lindenmayer.py
@@ -13,8 +13,19 @@ import turtle
 def test(alpha):
     alpha['A'](100)
 
+def expand(axiom, rules, n):
+    if n == 0:
+        return axiom
+    else:
+        return ''.join([expand(rules[char], rules, n - 1) for char in axiom])
 
 
+def draw(string, alphabet, length, angle, stack=[]):
+    for (step, scale) in [alphabet[char] for char in string]:
+        #step()
+        pass
+# TODO: add trait to distinguish btwn a move and a pivot!
+# TODO: ADD DOCUMENTATION, or it'll all be forgotten/unreadable
 
 if __name__ == '__main__':
 
@@ -24,5 +35,11 @@ if __name__ == '__main__':
     # form: KEY : (function, param scale)
     # relies on a standard length/angle ---> altered  by param scale
     alphabet = {'A': franklin.forward}
+    rules = {'A': 'AB',
+             'B': 'A'}
 
-    test(alphabet)
+    #test(alphabet)
+
+    expanded = expand('A', rules, 5)
+
+    draw('',alphabet,10,10)
