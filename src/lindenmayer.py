29b2c95eded008859d6c33617934a8aa16c39fbf
Jake Cariello <jakecariello@Jakes-MacBook-Pro.local>
2018-10-11 01:50:59 -0400

    more edits to lindenmayer... got preliminary buggy drawing

--- src/lindenmayer.py
+++ src/lindenmayer.py
@@ -10,36 +10,87 @@ Purpose: Draw an L-system using an axiom string and angle (∂).
 import turtle
 
 # %% Define functions
-def test(alpha):
-    alpha['A'](100)
+
 
 def expand(axiom, rules, n):
     if n == 0:
         return axiom
     else:
-        return ''.join([expand(rules[char], rules, n - 1) for char in axiom])
+        return ''.join([expand(rules.get(char, ''), rules, n - 1) for char in axiom])
 
 
-def draw(string, alphabet, length, angle, stack=[]):
-    for (step, scale) in [alphabet[char] for char in string]:
-        #step()
-        pass
-# TODO: add trait to distinguish btwn a move and a pivot!
-# TODO: ADD DOCUMENTATION, or it'll all be forgotten/unreadable
+def draw(string, alphabet, types, n, stack=[]):
+
+    """
+    NOTE: Uses recursion!!!
+
+    :param n: the current layer of recursion
+    :param string: STRING containing characters in the alphabet. Each character corresponds to a command.
+    :param alphabet: DICT with the character as the key, then a tuple of the command type,
+                        the actual function to be called, and a scale factor (relating to previous iteration)
+                        NOTE: the scale should default to 1 if not specified?
+    :param types: DICT with a command type (str) as key, and the associated reference magnitude (float) as value
+    :param stack: LIST containing tuples of (x, y, length, angle). Opening bracket signals entry
+                    into new push, and a closed bracket pops the last tuple so drawing may resume from before pushed.
+                    NOTE: expects that the command of a push type will return its position and direction in
+                    a 3-term tuple. The command of a pop type should both set the position and angle from the push data.
+    :return: the current string and stack
+    """
+
+    for char in string:
+        string = string[1 : len(string)]
+        (kind, command, scale) = alphabet[char]
+
+        if kind is 'push':
+            stack.append(command(types[kind]))  # should return (x, y, angle)
+            #print(stack)
+            n += 1
+
+        elif kind is 'pop':
+            command(types[kind], stack.pop())  # should go to x, y and turn to angle
+            #print(stack)
+            n -= 1
+
+        elif kind is 'none':
+            pass
+
+        else:
+            command(types[kind] * scale)
+
+
+def get_state(t: turtle.Turtle):
+    return t.xcor(), t.ycor(), t.heading()
+
+
+def set_state(t: turtle.Turtle, state: tuple):
+    print('GO TO {}'.format(state))
+    t.penup()
+    t.setposition(state[0], state[1])
+    t.setheading(state[2])
+    t.pendown()
 
 if __name__ == '__main__':
 
     window = turtle.Screen()
     franklin = turtle.Turtle()
+    franklin.speed(0)
 
-    # form: KEY : (function, param scale)
-    # relies on a standard length/angle ---> altered  by param scale
-    alphabet = {'A': franklin.forward}
-    rules = {'A': 'AB',
-             'B': 'A'}
+    rules = {'X': 'F+[[X]-X]-F[-FX]+X',
+             'F': 'FF'}
+    axiom = 'X'
 
-    #test(alphabet)
+    expanded = expand(axiom, rules, 3)
 
-    expanded = expand('A', rules, 5)
+    types = {'move': 10,
+             'turn': 25,
+             'push': franklin,
+             'pop': franklin}
 
-    draw('',alphabet,10,10)
+    alphabet = {'X': ('none', None, None),
+                'F': ('move', franklin.forward, 1),
+                '-': ('turn', franklin.left, 1),
+                '+': ('turn', franklin.left, -1),
+                '[': ('push', get_state, franklin),
+                ']': ('pop', set_state, franklin)}
+
+    draw(expanded, alphabet, types, 3)
